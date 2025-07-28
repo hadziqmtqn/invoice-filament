5c8557f502cd00a83e8d4a4a16fa9bd9608f1df4
Hadziq <36812793+hadziqmtqn@users.noreply.github.com>
2025-07-28 16:39:08 +0700

    Add media library support and update S3 config
    Introduced Spatie Laravel Media Library and Filament media library plugin dependencies. Added migration for the media table. Updated S3 filesystem configuration and environment variables to use generic object storage keys. Simplified root route redirect.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return to_route('filament.panel.auth.login');
-});
+Route::get('/', fn() => redirect()->to('/panel/login'));
